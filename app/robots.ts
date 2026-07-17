fe1cb92eaf86f4ccd102b98a65829bf7c4b0f377
817hitomi <17hitomi@gmail.com>
2026-07-17 12:52:45 +0800

    廣告

--- app/robots.ts
+++ app/robots.ts
@@ -6,7 +6,7 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
-      allow: "/",
+      allow: ["/", "/ads.txt"],
       disallow: ["/admin", "/api"]
     },
     sitemap: `${siteUrl}/sitemap.xml`
